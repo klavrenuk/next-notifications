81718f84ca78be19b629d16757437d644c7b4101
klavrenuk <klavrenuk1993@gmail.com>
2025-10-02 11:55:04 +0300

    init

---
+++ notifications/src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/styles/home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+      Home page
+    </div>
+  );
+}
---
+++ notifications/src/app/types/user.ts
@@ -0,0 +1,5 @@
+export interface User {
+    id: number,
+    img?: string;
+    money: string;
+}
