d6aac6905bdc8093d006aec9294b6ebb92e07936
klavrenuk <klavrenuk1993@gmail.com>
2025-10-02 12:39:56 +0300

    Empty

--- notifications/src/app/components/empty/Empty.tsx
+++ notifications/src/app/components/empty/Empty.tsx
@@ -1,7 +1,18 @@
+import styles from './styles/empty.module.css'
+
+import Image from 'next/image';
+
 export default function Empty() {
     return (
-        <div>
-            Empty
+        <div className={styles.empty}>
+            <Image
+                src="/images/photo.jpg"
+                alt="Пока нет информации"
+                width={260}
+                height={200}
+            />
+            <h6 className={styles.title}>Пока нет информации</h6>
+            <p>Репосты, отметки «Нравится» и многое другое — здесь вы найдете все взаимодействия с контентом.</p>
         </div>
     )
 }
