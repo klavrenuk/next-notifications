db47d2e609b617a174c7c54a200ae7ee5b7997ae
klavrenuk <klavrenuk1993@gmail.com>
2025-10-02 12:09:44 +0300

    Notifications: верстка компонентов

---
+++ notifications/src/app/components/titles/SectionTitle.tsx
@@ -0,0 +1,9 @@
+import styles from './styles/section-title.module.css'
+
+export default function SectionTitle() {
+    return (
+        <h2 className={styles.title}>
+            Page title
+        </h2>
+    )
+}
--- notifications/src/app/pages/notifications/page.tsx
+++ notifications/src/app/pages/notifications/page.tsx
@@ -1,7 +1,13 @@
+import SectionTitle from "@/app/components/titles/SectionTitle"
+import NavPage from "@/app/components/navigation/NavPage"
+import Empty from "@/app/components/empty/Empty"
+
 export default function Notifications() {
     return (
         <section className="">
-            Page notifcations
+            <SectionTitle text="Уведомления" />
+            <NavPage />
+            <Empty />
         </section>
     )
 }
